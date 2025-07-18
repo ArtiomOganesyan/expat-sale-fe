6e5e757fe002ed752f7e6e1651ad14fd9be2e86a
Artem Molchanov <artem700@gmail.com>
2025-07-18 08:34:08 +0300

    typed code in currencApi added currencySettings features

---
+++ src/features/settings/Settings.tsx
@@ -0,0 +1,11 @@
+import CurrencySettings from "./ui/CurrencySettings"
+
+const Settings = () => {
+  return (
+    <div>
+      <CurrencySettings />
+    </div>
+  )
+}
+
+export default Settings
--- src/pages/Settings/SettingsPage.tsx
+++ src/pages/Settings/SettingsPage.tsx
@@ -1,45 +1,12 @@
-import {
-  Box,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material"
-import { useState } from "react"
-import { useGetCurrencyRateQuery } from "../../entities/currency/currencyAPI"
 import styles from "./SettingsPage.module.css"
-
-const LOCAL_STORAGE_KEY = "userCurrency"
+import Settings from "../../features/settings/Settings"
 
 const SettingsPage = () => {
-  const { data: rates = []} = useGetCurrencyRateQuery(undefined)
-  const [selectedCurrency, setSelectedCurrency] = useState<string>(()=> localStorage.getItem(LOCAL_STORAGE_KEY)?? "")
-
-
-  const handleChange = (event: any) => {
-    const value = event.target.value
-    setSelectedCurrency(value)
-    localStorage.setItem(LOCAL_STORAGE_KEY, value)
-  }
-
   return (
     <div className={styles.container}>
-    <h2>Settings</h2>
-    <FormControl fullWidth>
-      <InputLabel id="currency-select-label">Select Currency</InputLabel>
-      <Select
-        labelId="currency-select-label"
-        value={selectedCurrency}
-        onChange={handleChange}
-      >
-        {rates.map((rate: any) => (
-          <MenuItem key={rate.id} value={rate.iso_4217}>
-            {rate.symbol} {rate.iso_4217}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
-  </div>
+      <h2>Settings</h2>
+      <Settings />
+    </div>
   )
 }
 

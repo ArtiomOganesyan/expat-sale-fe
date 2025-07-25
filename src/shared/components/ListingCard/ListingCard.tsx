f4a9f876b3abc2abd13673153b5249929d4e1bed
Artem Molchanov <artem700@gmail.com>
2025-07-25 19:55:13 +0300

    added prices and rates

--- src/shared/components/ListingCard/ListingCard.tsx
+++ src/shared/components/ListingCard/ListingCard.tsx
@@ -1,42 +1,63 @@
 import Carousel from 'react-material-ui-carousel';
 import { Link } from 'react-router';
 import { type Item } from '../../../entities/items/items.type';
+import styles from './ListingCard.module.css';
 
 interface ListingCardProps {
   item: Item;
   url: string;
+  convertedPrice?: number | null;
+  convertedCurrency?: string | null;
 }
 
-function ListingCard({ item, url }: ListingCardProps) {
+function ListingCard({ item, url, convertedPrice, convertedCurrency }: ListingCardProps) {
+  const hasConverted = convertedPrice != null && convertedCurrency != null;
+  const isSameCurrency = item.currency === convertedCurrency;
+
   return (
-    <div style={{ borderBottom: '1px solid #ccc' }}>
+    <div className={styles.cardContainer}>
       {item.images?.length ? (
         <Carousel
           autoPlay={false}
-          animation='slide'
+          animation="slide"
           height={200}
           indicators={true}
         >
           {item.images.map(image => (
             <img
               key={image.public_url}
-              style={{
-                objectFit: 'contain',
-                height: '200px',
-                width: '100%',
-              }}
+              className={styles.carouselImage}
               src={image.public_url}
-              alt='product'
+              alt="product"
             />
           ))}
         </Carousel>
       ) : null}
-      <div style={{ marginTop: '1rem' }}>
-        <Link to={`${url}/${item.id}`}>
+
+      <div className={styles.cardContent}>
+        <Link to={`${url}/${item.id}`} className={styles.cardLink}>
           <div>{item.title}</div>
-          <div>
-            {item.price} {item.currency}
-          </div>
+
+          {hasConverted ? (
+            isSameCurrency ? (
+              <div className={styles.mainPrice}>
+                {convertedPrice} {convertedCurrency}
+              </div>
+            ) : (
+              <>
+                <div className={styles.mainPrice}>
+                  {item.price} {item.currency}
+                </div>
+                <div className={styles.convertedPrice}>
+                  ({convertedPrice} {convertedCurrency})
+                </div>
+              </>
+            )
+          ) : (
+            <div className={styles.mainPrice}>
+              {item.price} {item.currency}
+            </div>
+          )}
         </Link>
       </div>
     </div>

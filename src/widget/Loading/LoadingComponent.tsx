565afd6465c9cb6622b3760379666bc17781b4d9
Denis Barishpoltsev <63596272+Shtolz1983@users.noreply.github.com>
2025-08-26 19:54:31 +0300

    Center `LoadingComponent` using MUI's `Box` and improve SVG gradient styling.

--- src/widget/Loading/LoadingComponent.tsx
+++ src/widget/Loading/LoadingComponent.tsx
@@ -1,20 +1,34 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
-
-
+import { Box } from '@mui/material';
 
 export function LoadingComponent() {
   return (
-    <React.Fragment>
-      <svg width={0} height={0}>
+    <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+      <svg
+        width={0}
+        height={0}
+      >
         <defs>
-          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" stopColor="#bebebeff" />
-            <stop offset="100%" stopColor="#000000ff" />
+          <linearGradient
+            id='my_gradient'
+            x1='0%'
+            y1='0%'
+            x2='0%'
+            y2='100%'
+          >
+            <stop
+              offset='0%'
+              stopColor='#bebebeff'
+            />
+            <stop
+              offset='100%'
+              stopColor='#000000ff'
+            />
           </linearGradient>
         </defs>
       </svg>
       <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} />
-    </React.Fragment>
+    </Box>
   );
 }

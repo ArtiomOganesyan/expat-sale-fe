11ddee11e7410360cf50c4072400155df5ddec9e
artiom_oganesyan <artiomoganesyan@gmail.com>
2025-09-04 13:29:35 +0700

    contact website added, and telegram fix

--- src/pages/Item/utils/resolveUrl.ts
+++ src/pages/Item/utils/resolveUrl.ts
@@ -10,12 +10,25 @@ export const resolveUrl = (platform: PlatformType, contact: string) => {
       break;
     case 'telegram':
       const telegramUser = contact.startsWith('@') ? contact.slice(1) : contact;
-      window.open(`https://t.me/${telegramUser}`, '_blank');
+      // check if the telegramUser contains https://t.me/ or http://t.me/
+      if (telegramUser.startsWith('https://t.me/') || telegramUser.startsWith('http://t.me/')) {
+        window.open(telegramUser, '_blank');
+      } else {
+        window.open(`https://t.me/${telegramUser}`, '_blank');
+      }
       break;
     case 'email':
       window.open(`mailto:${contact}`, '_blank');
       break;
     case 'zalo':
+      break;
+    case 'website':
+      if (contact.startsWith('https://t.me/') || contact.startsWith('http://t.me/')) {
+        window.open(contact, '_blank');
+      } else {
+        window.open(`https://${contact}`, '_blank');
+      }
+      window.open(`https://${contact}`, '_blank');
       break;
     default:
       break;
